d3c5aea7c00f691c833186e1bcc0dbaaae46f6a8
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-03-21 18:30:52 +0000

    Deploying to gh-pages from @ AleksandraCherevko/goit-js-hw-09@b077513e4bb04f27fff36061285f8480417cc8c4 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/styles-cb79a739.js";const t=document.querySelector(".feedback-form");document.querySelector("textarea");t.addEventListener("input",e=>{e.preventDefault(),console.log(e.target.elements.message.value)});
+import"./assets/styles-cb79a739.js";const t=document.querySelector(".feedback-form");document.querySelector("textarea");t.addEventListener("inputтззщл",e=>{e.preventDefault(),console.log(e.target.elements.message.value)});
 //# sourceMappingURL=commonHelpers2.js.map
