9d40a20c869b4e6fe21bc4aa9d87c6d6be573a4d
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-03-24 20:05:19 +0100

    upd

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,24 +2,27 @@ const STORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
 
-const textarea = form.querySelector('textarea');
-console.log(textarea);
-const emailarea = form.querySelector('input');
-console.log(emailarea);
+const textarea = form.elements.message;
+textarea.value = localStorage.getItem(STORAGE_KEY) ?? '';
+
+const emailarea = form.elements.email;
+emailarea.value = localStorage.getItem(STORAGE_KEY) ?? '';
+
+//
+
 let formValues = JSON.stringify(localStorage.getItem(STORAGE_KEY));
 
 form.addEventListener('input', event => {
-  localStorage.setItem(STORAGE_KEY, event.target.value);
+  localStorage.setItem(STORAGE_KEY, event);
 });
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  formValues = {
+  let formValues = {
     Email: form.elements.email.value,
     Message: form.elements.message.value,
   };
   console.log(formValues);
-
   localStorage.removeItem(STORAGE_KEY);
   form.reset();
 });
