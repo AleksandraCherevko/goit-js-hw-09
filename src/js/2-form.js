bc9da002a5a754e1d58fa18b8fda30e66c251d37
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-03-29 09:00:04 +0100

    upd

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,26 +1,59 @@
 const STORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea');
+const input = form.querySelector('input');
+popText();
 
-const textarea = form.elements.message;
-textarea.value = localStorage.getItem(STORAGE_KEY) ?? '';
+// let formValues = {
+//   Email: form.elements.email.value,
+//   Message: form.elements.message.value,
+// };
 
-const emailarea = form.elements.email;
-emailarea.value = localStorage.getItem(STORAGE_KEY) ?? '';
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
 
-let formValues = JSON.stringify(localStorage.getItem(STORAGE_KEY));
+//   localStorage.getItem(STORAGE_KEY, JSON.stringify(formValues));
+//   event.currentTarget.reset();
+//   localStorage.removeItem(STORAGE_KEY);
+// });
 
-form.addEventListener('input', event => {
-  localStorage.setItem(STORAGE_KEY, event.target.value);
-});
+// const textarea = form.elements.message;
+// textarea.value = localStorage.getItem(STORAGE_KEY) ?? '';
 
+// const emailarea = form.elements.email;
+// emailarea.value = localStorage.getItem(STORAGE_KEY) ?? '';
+
+// let formValues = JSON.stringify(localStorage.getItem(STORAGE_KEY));
 form.addEventListener('submit', event => {
   event.preventDefault();
-  let formValues = {
-    Email: form.elements.email.value,
-    Message: form.elements.message.value,
-  };
-  console.log(formValues);
+  event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
-  form.reset();
 });
+
+form.addEventListener('input', event => {
+  const formValues = {
+    Email: form.email.value,
+    Message: form.message.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formValues));
+});
+
+function popText() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const { email, message } = JSON.parse(savedMessage);
+  if (savedMessage) {
+    textarea.value = email;
+    input.value = message;
+  }
+}
+
+//   event.preventDefault();
+//   let formValues = {
+//     Email: form.elements.email.value,
+//     Message: form.elements.message.value,
+//   };
+//   console.log(formValues);
+//   localStorage.removeItem(STORAGE_KEY);
+//   form.reset();
+// });
