74a6dbb26f2af2cd908c2a0411197fec83aa4e9b
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-04-01 14:09:14 +0200

    upd

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,30 +5,10 @@ const textarea = form.querySelector('textarea');
 const input = form.querySelector('input');
 popText();
 
-// let formValues = {
-//   Email: form.elements.email.value,
-//   Message: form.elements.message.value,
-// };
-
-// form.addEventListener('submit', event => {
-//   event.preventDefault();
-
-//   localStorage.getItem(STORAGE_KEY, JSON.stringify(formValues));
-//   event.currentTarget.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-// });
-
-// const textarea = form.elements.message;
-// textarea.value = localStorage.getItem(STORAGE_KEY) ?? '';
-
-// const emailarea = form.elements.email;
-// emailarea.value = localStorage.getItem(STORAGE_KEY) ?? '';
-
-// let formValues = JSON.stringify(localStorage.getItem(STORAGE_KEY));
 form.addEventListener('submit', event => {
   event.preventDefault();
   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  console.log(savedMessage);
+
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 });
@@ -38,24 +18,19 @@ form.addEventListener('input', event => {
     email: form.email.value,
     message: form.message.value,
   };
+
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formValues));
+
+  if (!form.email.value || !form.message.value) {
+    return;
+  }
 });
 
 function popText() {
   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  // const { email, message } = JSON.parse(savedMessage);
+
   if (savedMessage) {
     textarea.value = savedMessage.message || '';
     input.value = savedMessage.email || '';
   }
 }
-
-//   event.preventDefault();
-//   let formValues = {
-//     Email: form.elements.email.value,
-//     Message: form.elements.message.value,
-//   };
-//   console.log(formValues);
-//   localStorage.removeItem(STORAGE_KEY);
-//   form.reset();
-// });
