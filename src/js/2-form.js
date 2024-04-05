9a3596bc6adfdb21732efbc41a902d8fba7dfe1b
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-04-05 11:13:15 +0200

    upd

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,17 +3,25 @@ const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 const textarea = form.querySelector('textarea');
 const input = form.querySelector('input');
+
 popText();
 
 form.addEventListener('submit', event => {
   event.preventDefault();
   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-  if (savedMessage) {
-    console.log(savedMessage);
-  } else {
+  if (!savedMessage) {
+    return alert('Please fill in all the fields!');
+  }
+  const formValues = {
+    email: form.email.value.trim(),
+    message: form.message.value.trim(),
+  };
+
+  if (!formValues.email === '' || !formValues.message === '') {
     return;
   }
+
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 });
@@ -23,9 +31,6 @@ form.addEventListener('input', event => {
     email: form.email.value.trim(),
     message: form.message.value.trim(),
   };
-  if (!form.email.value || !form.message.value) {
-    return;
-  }
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formValues));
 });
 
