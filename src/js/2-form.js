58859cb0a5f5252765ec4649a68423508d59f8c0
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-04-09 14:29:09 +0200

    upd

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,103 +1,42 @@
 const STORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
-const textarea = form.querySelector('textarea');
-const input = form.querySelector('input');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
 
-textarea.addEventListener('input', onTextareaInput);
+popText();
 
+form.addEventListener('submit', event => {
+  event.preventDefault();
 
+  const formValues = {
+    email: emailInput.value.trim(),
+    message: messageInput.value.trim(),
+  };
 
-// const value = localStorage.getItem(STORAGE_KEY);
-// console.log(JSON.parse(value));
+  if (formValues.email === '' || formValues.message === '') {
+    return alert('Please fill in all the fields!');
+  }
 
-// // popText();
+  console.log(formValues);
 
-// // form.addEventListener('submit', event => {
-// //   event.preventDefault();
-// //   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+});
 
-// //   const formValues = {
-// //     email: form.elements.email.value.trim(),
-// //     message: form.message.elements.value.trim(),
-// //   };
+form.addEventListener('input', event => {
+  const formValues = {
+    email: emailInput.value.trim(),
+    message: messageInput.value.trim(),
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formValues));
+});
 
-// //   if (formValues.email === '' || formValues.message === '') {
-// //     return alert('Please fill in all the fields!');
-// //   }
+function popText() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-// //   event.currentTarget.reset();
-// //   localStorage.removeItem(STORAGE_KEY);
-// // });
-
-// // form.addEventListener('input', event => {
-// //   const formValues = {
-// //     email: form.elements.email.value.trim(),
-// //     message: form.elements.message.value.trim(),
-// //   };
-// //   localStorage.setItem(STORAGE_KEY, JSON.stringify(formValues));
-// // });
-
-// // function popText() {
-// //   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-// //   // if (savedMessage) {
-// //   //   textarea.value = savedMessage.message || '';
-// //   //   input.value = savedMessage.email || '';
-// //   // }
-
-// //   // Перевірка на undefined перед заповненням полів вводу
-
-// //   if (savedMessage.message !== 'undefined') {
-// //     textarea.value = savedMessage.message;
-// //   } else if (typeof savedMessage.email !== 'undefined') {
-// //     input.value = savedMessage.email;
-// //   }
-// // }
-
-// const STORAGE_KEY = 'feedback-form-state';
-
-// const form = document.querySelector('.feedback-form');
-// const textarea = form.querySelector('textarea');
-// const input = form.querySelector('input');
-
-// popText();
-
-// form.addEventListener('submit', event => {
-//   event.preventDefault();
-//   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-//   const formValues = {
-//     email: form.elements.email.value.trim(), // Використовуємо form.elements з атрибутом name
-//     message: form.elements.message.value.trim(),
-//   };
-
-//   if (formValues.email === '' || formValues.message === '') {
-//     return alert('Please fill in all the fields!');
-//   }
-
-//   console.log(formValues);
-
-//   event.currentTarget.reset();
-//   localStorage.removeItem(STORAGE_KEY);
-// });
-
-// form.addEventListener('input', event => {
-//   const formValues = {
-//     email: form.elements.email.value.trim(),
-//     message: form.elements.message.value.trim(),
-//   };
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formValues));
-// });
-
-// function popText() {
-//   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-//   if (savedMessage) {
-//     if (typeof savedMessage.message !== 'undefined') {
-//       textarea.value = savedMessage.message;
-//     } else if (typeof savedMessage.email !== 'undefined') {
-//       input.value = savedMessage.email;
-//     }
-//   }
-// }
+  if (savedMessage) {
+    emailInput.value = savedMessage.email || '';
+    messageInput.value = savedMessage.message || '';
+  }
+}
